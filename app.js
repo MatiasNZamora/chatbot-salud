9cadf18bd0ab49950ee27209b2aa27ecf0a62d3b
Matias N Zamora <info@matiasnzamora.com.ar>
2023-04-11 08:32:07 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,111 @@
+const { createBot, createProvider, createFlow, addKeyword, addAnswer } = require('@bot-whatsapp/bot')
+
+const QRPortalWeb = require('@bot-whatsapp/portal')
+const BaileysProvider = require('@bot-whatsapp/provider/baileys')
+const MySQLAdapter = require('@bot-whatsapp/database/mysql')
+
+/**
+ * Declaramos las conexiones de MySQL
+ */
+const MYSQL_DB_HOST = 'localhost'
+const MYSQL_DB_USER = 'root'
+const MYSQL_DB_PASSWORD = ''
+const MYSQL_DB_NAME = 'bot'
+const MYSQL_DB_PORT = '3306'
+
+/**
+ * Aqui declaramos los flujos hijos, los flujos se declaran de atras para adelante, es decir que si tienes un flujo de este tipo:
+ *
+ *          Menu Principal
+ *           - SubMenu 1
+ *             - Submenu 1.1
+ *           - Submenu 2
+ *             - Submenu 2.1
+ *
+ * Primero declaras los submenus 1.1 y 2.1, luego el 1 y 2 y al final el principal.
+*/
+
+const flowAdios = addKeyword(
+    ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','ñ','o','p','q','r','s','t','u','v','w','x','y','z']
+    ).addAnswer(
+    '❤ Gracias por contactarte, te brindaremos una respuesta dentro de las próximas 24hs.',
+    'Tu salud nos interesa❤.'
+)
+
+const barrio = addKeyword(['1', 'uno','2','dos','3','tres','4','cuatro']).addAnswer(
+    ['🏠 ¿En que barrio y calle vivis?'],
+    null,
+    null,
+    [flowAdios]
+)
+
+const flowHorario = addKeyword(['1', 'uno','2','dos','3','tres','4','cuatro']).addAnswer(
+    [
+        '🕐¿En que horario te podemos contactar? \n',
+        '🌅 *1*.  Cualquier hora',
+        '⏰ *2*.   Mañana',
+        '🌞 *3*.   Tarde',
+        '🌜 *4*.   Noche',
+    ],    
+    null,
+    null,
+    [barrio]
+)
+
+const flowFiebre = addKeyword(['1', 'uno', 'Siento Fiebre']).addAnswer(
+    [
+        '🌡 ¿Desde Cuando tenes Fiebre? \n',
+        '✍🏼 *1*.  24hs',
+        '✍🏼 *2*.  2 dias',
+        '✍🏼 *3*.  3 dias',
+        '✍🏼 *4*.  Mas de 5 dias',
+    ],    
+    null,
+    null,
+    [flowHorario]
+)
+
+const flowSecundario = addKeyword('Menu').addAnswer(
+    [
+        'Escribe el numero: \n',
+        '🌡 *1*.  Siento Fiebre',
+        '💧 *2*.  Me Siento Triste',
+        '💊 *3*.  Nesecito Medicamentos'
+    ],
+    null,
+    null,
+    [flowFiebre]
+)
+
+
+const flowPrincipal = addKeyword(['hola', 'buen dia', 'hol', 'holis']).addAnswer(
+    [
+        'Hola! Soy tu guía virtual de salud? 🩺',
+        'Contame, En que Podemos ayudarte?',
+        '\n ✍🏼 Si queres conocer las Opciones de consultas escribi *Menu*'
+    ],
+    null,
+    null,
+    [flowSecundario]   
+)
+
+
+const main = async () => {
+    const adapterDB = new MySQLAdapter({
+        host: MYSQL_DB_HOST,
+        user: MYSQL_DB_USER,
+        database: MYSQL_DB_NAME,
+        password: MYSQL_DB_PASSWORD,
+        port: MYSQL_DB_PORT,
+    })
+    const adapterFlow = createFlow([flowPrincipal])
+    const adapterProvider = createProvider(BaileysProvider)
+    createBot({
+        flow: adapterFlow,
+        provider: adapterProvider,
+        database: adapterDB,
+    })
+    QRPortalWeb()
+}
+
+main()
